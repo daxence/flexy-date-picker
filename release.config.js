9da96ad592b42ae8f2cff2384f57100545f5731c
daxence <wissam.saidi.daif@gmail.com>
2026-09-12 17:43:52 +0200

    fix: ci pipelines issue

--- release.config.js
+++ release.config.js
@@ -12,6 +12,6 @@ export default {
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
-    '@semantic-release/github',
+    ['@semantic-release/github', { labels: false }],
   ],
 };
